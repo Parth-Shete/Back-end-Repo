006a502b06f6eefa97bf9af0a7120ba99a88890b
dhananjaykuber <dnkuber2002@gmail.com>
2022-11-25 23:07:15 +0530

    after error resolved

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-const userRoutes = require('./backend/routes/user');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
