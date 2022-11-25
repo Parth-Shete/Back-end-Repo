b1675763cb925ea3217aed595ea12b35d2da238e
dhananjaykuber <dnkuber2002@gmail.com>
2022-11-25 23:13:30 +0530

    after adding cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // routes
